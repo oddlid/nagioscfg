a1e476a3f6910b970318c553a9512b646bc7736e
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2016-08-23 16:38:28 +0200

    Notes to self on how to go on with IO stuff

--- io.go
+++ io.go
@@ -5,17 +5,31 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"unicode"
 )
+
+func IsComment(buf []byte) bool {
+	// We might need to check for and skip whitespace before #, but for now, this will do
+	return bytes.IndexByte(buf, '#') == 0
+}
+
+func IsBlankLine(buf []byte) bool {
+	return unicode.IsSpace(buf[0]) // bogus, change me
+}
 
 func ReadKeyVal() {
 }
 
+// - check for comments and discard
+// - check for blank lines and discard
+// - check for beginning of a definition and enter a mode/set a flag
+// - check for end of definition if "within-flag" set, unset flag if '}' encountered
+// - split lines into key/value if within object definition
+// - feed result to a consumer that creates object instances
 func Read(r io.Reader) {
 	scanner := bufio.NewScanner(r)
-	var buf []byte
 	for scanner.Scan() {
-		buf = scanner.Bytes()
-		if bytes.IndexByte(buf, '#') == 0 {
+		if IsComment(scanner.Bytes()) {
 			//fmt.Printf("Seems we have a comment: %q\n", buf)
 			continue
 		}
